fcf1d14374b9e09116cc670e2c39edf989b0f679
Kahina Ouali <kahinaouali.pro@gmail.com>
2022-07-06 15:01:14 +0200

    Intégration du header

---
+++ public/js/header.js
@@ -0,0 +1,9 @@
+function menuOpen() {
+    document.getElementById("nav-cnt").style.height = "100%";
+    document.getElementById("nav-cnt").style.width = "100%";
+}
+
+function closeOpen() {
+    document.getElementById("nav-cnt").style.height = "0%";
+    document.getElementById("nav-cnt").style.width = "0%";
+}
